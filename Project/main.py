49b1f5b851647d88c113aedbce0d66bd563d11aa
Jerome <jeertmans@icloud.com>
2020-04-16 22:53:10 +0200

    ching chong

--- Project/main.py
+++ Project/main.py
@@ -9,15 +9,24 @@ def plot_mesh(filename='data/mesh.txt'):
     data = np.loadtxt(filename, delimiter=',')
     n_x = 360
     n_y = 491
-    print(data.shape)
-    x = np.reshape(data[:, 0], (n_x, n_y))
-    y = np.reshape(data[:, 1], (n_x, n_y))
-    val_1 = np.reshape(data[:, 2], (n_x, n_y))
-    val_2 = np.reshape(data[:, 3], (n_x, n_y))
+    x = np.reshape(data[:, 0], (n_x, n_y), order='F')
+    y = np.reshape(data[:, 1], (n_x, n_y), order='F')
+    val_1 = np.reshape(data[:, 2], (n_x, n_y), order='F')
+    val_2 = np.reshape(data[:, 3], (n_x, n_y), order='F')
+
+
+    H = 20
+    R = 1
+    r = np.sqrt(x*x + y*y)
+    theta = np.arctan2(y, x)
+    a = (R-r) * np.pi / H
+    phi = np.sin(a) * np.sin(a) * np.cos(2*theta)
+
+    error = abs(val_2 - phi)
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(x, y, val_2)
+    ax.plot_surface(x, y, error, cmap='plasma')
     plt.show()
 
 
