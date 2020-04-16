350fd0574b5e3f571ec7031ab7bafa3a4438599a
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-04-16 14:50:27 +0200

    Grad of Phi or P

--- Project/inc/solver.h
+++ Project/inc/solver.h
@@ -6,6 +6,7 @@
 
 // Function declaration
 void compute_rhs(MACMesh *mesh, double *result, double dt);
+void compute_grad_scalar(MACMesh *mesh, double *res_x, double *res_y, int p_or_phi);
 
 
 #endif
--- Project/src/solver.c
+++ Project/src/solver.c
@@ -47,3 +47,54 @@ void compute_rhs(MACMesh *mesh, double *result, double dt) {
     }
 
 }
+
+
+/*
+ *  Computes the pressure gradient.
+ *  On the border, we do not calculate those values: the velocities are known,
+ *  therefore we do not need it.
+ *  p_or_phi > 0 -> grad(pressure)
+ *  p_or_phi < 0 -> grad(phi)
+ */
+void compute_grad_scalar(MACMesh *mesh, double *res_x, double *res_y, int p_or_phi) {
+    double *field;
+    if (p_or_phi > 0) {
+        field = mesh->p->val1;
+    } else {
+        field = mesh->p->val2;
+    }
+
+    // Init some vars
+    int ind;
+    double d1 = mesh->p->d1;
+    double d2 = mesh->p->d2;
+    doublle h1, h2;
+
+    // First, compute in the x-direction
+    for (int i = 1; i < mesh->u->n1-1; i++) {
+        for (int j = 0; j < mesh->u->n2; j++) {
+            ind = i*mesh->u->n2 + j;
+            h1 = mesh->u->h1[ind];
+            h2 = mesh->u->h2[ind];
+
+            res_x[ind] = (field[(i+1)*mesh->p->n2+j] - field[i*mesh->p->n2+j]) / (d1*h1);
+        }
+    }
+
+    // Then compute in the y-direction
+    for (int i = 1; i < mesh->v->n1-1; i++) {
+        for (int j = 0; j < mesh->v->n2; j++) {
+            ind = i*mesh->v->n2 + j;
+            h1 = mesh->v->h1[ind];
+            h2 = mesh->v->h2[ind];
+
+            if (j == mesh->v->n2-1) {
+                res_y[ind] = (field[i*mesh->p->n2]       - field[i*mesh->p->n2+j]) / (d2*h2);
+            } else {
+                res_y[ind] = (field[i*mesh->p->n2+(j+1)] - field[i*mesh->p->n2+j]) / (d2*h2);
+            }
+        }
+    }
+
+
+}
